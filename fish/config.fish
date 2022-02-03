30f07fea7d564b08fc0558f4cb32cc60f05cd4bd
efugier <mail@emilienfugier.net>
2022-02-03 14:59:50 +0100

    feat: switch to helix editor

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,18 @@
-# 
-set -Ux EDITOR kak
-set -Ux GIT_EDITOR kak
+# system
+set -Ux EDITOR hx
+set -Ux GIT_EDITOR hx
 set -Ux MYTERM alacritty
 
+
+# Helix
+set -Ux HELIX_RUNTIME "$HOME/.config/helix/runtime"
+
 # Rust
-set PATH $HOME/.cargo/bin $PATH
+set -Ux PATH $HOME/.cargo/bin $PATH
 
 # Go
-set GOPATH "$HOME/go:$HOME/code"
-set PATH "$PATH:$GOPATH/bin"
+set -Ux GOPATH "$HOME/go:$HOME/code"
+set -Ux PATH "$PATH:$GOPATH/bin"
+set -Ux PATH "$PATH:/usr/local/go/bin"
 
 eval (starship init fish)
