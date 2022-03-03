975dd88123966d249ecc715f0ab75d7dbd96d117
efugier <mail@emilienfugier.net>
2022-03-03 18:00:53 +0100

    pyenv config + gl = `git graphlog`

--- fish/config.fish
+++ fish/config.fish
@@ -15,4 +15,14 @@ set -Ux GOPATH "$HOME/go:$HOME/code"
 set -Ux PATH "$PATH:$GOPATH/bin"
 set -Ux PATH "$PATH:/usr/local/go/bin"
 
+# Pyenv
+set -x PATH $HOME/.pyenv/bin $PATH
+pyenv init --path | source
+pyenv init - | source
+set -x PIPENV_PYTHON "$HOME/.pyenv/shims/python"
+set -x PIPENV_VENV_IN_PROJECT 1 # optional but recommended
+
+# httpie
+alias https='http --default-scheme=https'
+
 eval (starship init fish)
